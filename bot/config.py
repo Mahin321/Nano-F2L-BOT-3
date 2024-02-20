6d1df36d007b423ad2ab775e002b1eaff26d4d4f
Mahin321 <112240772+Mahin321@users.noreply.github.com>
2024-02-21 03:31:52 +0600

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,17 +1,17 @@
 from os import environ as env
 
 class Telegram:
-    API_ID = int(env.get("TELEGRAM_API_ID", 1234))
-    API_HASH = env.get("TELEGRAM_API_HASH", "xyz")
-    OWNER_ID = int(env.get("OWNER_ID", 1234567890))
+    API_ID = int(env.get("TELEGRAM_API_ID", ""))
+    API_HASH = env.get("TELEGRAM_API_HASH", "")
+    OWNER_ID = int(env.get("OWNER_ID", ""))
     ALLOWED_USER_IDS = env.get("ALLOWED_USER_IDS", "").split()
-    BOT_USERNAME = env.get("TELEGRAM_BOT_USERNAME", "BotFather")
-    BOT_TOKEN = env.get("TELEGRAM_BOT_TOKEN", "1234:abcd")
-    CHANNEL_ID = int(env.get("TELEGRAM_CHANNEL_ID", -1001234567890))
+    BOT_USERNAME = env.get("TELEGRAM_BOT_USERNAME", "")
+    BOT_TOKEN = env.get("TELEGRAM_BOT_TOKEN", "")
+    CHANNEL_ID = int(env.get("TELEGRAM_CHANNEL_ID", ""))
     SECRET_CODE_LENGTH = int(env.get("SECRET_CODE_LENGTH", 12))
 
 class Server:
-    BASE_URL = env.get("BASE_URL", "http://127.0.0.1:8080")
+    BASE_URL = env.get("BASE_URL", "")
     BIND_ADDRESS = env.get("BIND_ADDRESS", "0.0.0.0")
     PORT = int(env.get("PORT", 8080))
 
