dbf6d26ef5c2758fd099280585c4316dba247490
Mahin321 <112240772+Mahin321@users.noreply.github.com>
2024-02-22 19:13:11 +0600

    Update static.py

--- bot/modules/static.py
+++ bot/modules/static.py
@@ -1,14 +1,14 @@
 WelcomeText = \
-"""
+
 <b>👋 Hᴇʏ, </b>{}\n 
 <b>I'ᴍ ᴛᴇʟᴇɢʀᴀᴍ ғɪʟᴇs sᴛʀᴇᴀᴍɪɴɢ ʙᴏᴛ ᴀs ᴡᴇʟʟ ᴅɪʀᴇᴄᴛ ʟɪɴᴋs ɢᴇɴᴇʀᴀᴛᴏʀ</b>
 - /start to get this message.
 - /info to get user info.
 - /log to get bot logs. (admin only!)
-"""
+
 
 UserInfoText = \
-"""
+
 **First Name:**
 `{sender.first_name}`
 
@@ -20,27 +20,27 @@ UserInfoText = \
 
 **Username:**
 `@{sender.username}`
-"""
+
 
 FileLinksText = \
-"""
+
 <i><u>𝗬𝗼𝘂𝗿 𝗟𝗶𝗻𝗸 𝗚𝗲𝗻𝗲𝗿𝗮𝘁𝗲𝗱 !</u></i>\n
 <b>📂 Fɪʟᴇ ɴᴀᴍᴇ :</b> <b>{}</b>\n
 <b>📦 Fɪʟᴇ ꜱɪᴢᴇ :</b> <code>{}</code>\n
 <b>📥 Dᴏᴡɴʟᴏᴀᴅ :</b> <code>{}</code>\n
 <b>🖥 Wᴀᴛᴄʜ :</b> <code>{}</code>\n
 <b>🔗 Sʜᴀʀᴇ :</b> <code>{}</code>\n
-"""
+
 
 MediaLinksText = \
-"""
+
 <i><u>𝗬𝗼𝘂𝗿 𝗟𝗶𝗻𝗸 𝗚𝗲𝗻𝗲𝗿𝗮𝘁𝗲𝗱 !</u></i>\n
 <b>📂 Fɪʟᴇ ɴᴀᴍᴇ :</b> <b>{}</b>\n
 <b>📦 Fɪʟᴇ ꜱɪᴢᴇ :</b> <code>{}</code>\n
 <b>📥 Dᴏᴡɴʟᴏᴀᴅ :</b> <code>{}</code>\n
 <b>🖥 Wᴀᴛᴄʜ :</b> <code>{}</code>\n
 <b>🔗 Sʜᴀʀᴇ :</b> <code>{}</code>\n
-"""
+
 
 InvalidQueryText = \
 """
